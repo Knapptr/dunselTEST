5c3cb62aab3456c0cc927988fb2c43b70498e644
Tyler Knapp <knapptr@gmail.com>
2020-03-11 11:00:51 -0700

    Updates

---
+++ precache-manifest.193bdd4c598d81c6f466180c55ddaf2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad26a3d99759027ce598112baa9e36f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc37c1d45325aae7af3a",
+    "url": "/static/css/main.966ddb44.chunk.css"
+  },
+  {
+    "revision": "8af123cd8604cb31801e",
+    "url": "/static/js/2.0f0123e7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0f0123e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc37c1d45325aae7af3a",
+    "url": "/static/js/main.edbc5d07.chunk.js"
+  },
+  {
+    "revision": "bb284071e897cb8eed3b",
+    "url": "/static/js/runtime-main.d724832b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
